a6dc824c1dc4f2b80b02167b636c31afb60a163b
gbernalavila <gbernalavila@deloitte.com>
2024-08-12 12:09:57 -0600

    first commit

---
+++ src/app/course-title/course-title.component.ts
@@ -0,0 +1,19 @@
+import {Component, CUSTOM_ELEMENTS_SCHEMA, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'course-title',
+  standalone:true,
+  templateUrl: './course-title.component.html',
+  styleUrls: ['./course-title.component.css']
+})
+export class CourseTitleComponent implements OnInit {
+
+   @Input()
+  title:string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/courses/course-image/course-image.component.ts
@@ -0,0 +1,21 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'course-image',
+  standalone:true,
+  templateUrl: './course-image.component.html',
+  styleUrls: ['./course-image.component.css']
+})
+export class CourseImageComponent implements OnInit {
+
+  @Input('src')
+  imageUrl:string;
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
